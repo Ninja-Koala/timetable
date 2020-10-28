ea1de96a9678b9bd17975a2cf8008acbfd8bb3d2
Ninja-Koala <mail@ninjakoa.la>
2020-10-28 17:08:52 +0100

    Tidy up a bit, put most magic value at beginning

--- timetable.py
+++ timetable.py
@@ -4,6 +4,23 @@ import json, sys
 input_file=""
 output_file=""
 
+column_width=350
+
+weekday_y_pos=80
+
+border_x=40
+border_y=40
+
+box_end_y=1080
+
+text_line_spacing=40
+
+hor_line_pos=100
+
+font_size=36
+
+stroke_width=6
+
 if len(sys.argv)<2:
 	print("Usage: ./timetable.py input_json_file [output_svg_file]")
 	exit(1)
@@ -28,35 +45,35 @@ except:
 	print("Couldn't open file")
 	exit(1)
 
-column_width=300
+svg_text="\t<text font-family=\"Monospace\" x=\"{0}\" y=\"{1}\" fill=\"black\" style=\"font-size:{2}px\" text-anchor=\"middle\">{3}</text>\n"
 
-svg_text="\t<text font-family=\"Monospace\" x=\"{0}\" y=\"{1}\" fill=\"black\" style=\"font-size:36px\" text-anchor=\"middle\">{2}</text>\n"
+svg_box="M{0} {1} L{2} {1} L{2} {3} L{0} {3} Z"
+svg_hor_line="M{0} {1} L{2} {1}"
+svg_ver_line="\t\t\tM{0} {1} L{0} {2}\n"
 
-svg_box="M40 40 L{0} 40 L{0} 1080 L40 1080 Z"
-svg_hor_line="M40 100 L{0} 100"
-svg_ver_line="\t\t\tM{0} 40 L{0} 1080\n"
-
+ver_line_pos=border_x+column_width
 
 svg_ver_lines=""
-lines_pos=340
 for i in range(len(timetable["week"])-1):
-	svg_ver_lines+=svg_ver_line.format(lines_pos)
-	lines_pos+=column_width
+	svg_ver_lines+=svg_ver_line.format(ver_line_pos,border_y,box_end_y)
+	ver_line_pos+=column_width
 
-svg_box=svg_box.format(lines_pos)
-svg_hor_line=svg_hor_line.format(lines_pos)
+svg_box=svg_box.format(border_x,border_y,ver_line_pos,box_end_y)
+svg_hor_line=svg_hor_line.format(border_x,hor_line_pos,ver_line_pos)
+
+text_x_pos=border_x+column_width/2
 
 full_text=""
-text_pos=200
 for day in timetable["week"]:
-	full_text+=svg_text.format(text_pos,80,day["name"])
+	full_text+=svg_text.format(text_x_pos,weekday_y_pos,font_size,day["name"])
 	for event in day["events"]:
+		#TODO: better (configurable) time scaling
 		ver_pos=140+(int(event["time"]["hour"])*120+int(event["time"]["minute"])-960)*0.75
-		full_text+=svg_text.format(text_pos,ver_pos+00,event["time"]["hour"] + ":" + event["time"]["minute"])
-		full_text+=svg_text.format(text_pos,ver_pos+40,event["course"])
-		full_text+=svg_text.format(text_pos,ver_pos+80,event["name"])
-		full_text+=svg_text.format(text_pos,ver_pos+120,"("+event["room"]+")")
-	text_pos+=column_width
+		full_text+=svg_text.format(text_x_pos,ver_pos+0*text_line_spacing,font_size,event["time"]["hour"] + ":" + event["time"]["minute"])
+		full_text+=svg_text.format(text_x_pos,ver_pos+1*text_line_spacing,font_size,event["course"])
+		full_text+=svg_text.format(text_x_pos,ver_pos+2*text_line_spacing,font_size,event["name"])
+		full_text+=svg_text.format(text_x_pos,ver_pos+3*text_line_spacing,font_size,"("+event["room"]+")")
+	text_x_pos+=column_width
 
 
 svg_path="""\"
@@ -67,12 +84,12 @@ svg_path="""\"
 
 template="""\
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg height="1120" width="{0}" version="1.1" xmlns="http://www.w3.org/2000/svg">
+<svg height="{0}" width="{1}" version="1.1" xmlns="http://www.w3.org/2000/svg">
 	<rect x="0" y="0" width="100%" height="100%" fill="white"/>
-	<path d={1}
-	style="fill:none;stroke:black;stroke-width:6" />
-{2}</svg>
-""".format(lines_pos+40,svg_path,full_text)
+	<path d={2}
+	style="fill:none;stroke:black;stroke-width:{3}" />
+{4}</svg>
+""".format(box_end_y+border_y,ver_line_pos+border_x,svg_path,stroke_width,full_text)
 
 try:
 	svg_file=open(output_file,"w")
